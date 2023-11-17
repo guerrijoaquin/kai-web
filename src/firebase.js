2858a4b66225bccd2227bbb1b8ee844f18f375cf
Joaquin Guerrini <74384234+guerrijoaquin@users.noreply.github.com>
2023-11-16 21:38:03 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANL_LHC6jhS4vAA1-DrAO4-bmc_mm3Bf8",
-  authDomain: "kai-system-a754d.firebaseapp.com",
-  projectId: "kai-system-a754d",
-  storageBucket: "kai-system-a754d.appspot.com",
-  messagingSenderId: "653052123492",
-  appId: "1:653052123492:web:c72805c3cb20fbd883c271",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 export const app = initializeApp(firebaseConfig);
